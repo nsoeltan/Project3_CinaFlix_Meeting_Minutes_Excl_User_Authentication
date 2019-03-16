d9ad5da91fc9ecb7ac35d3208a03592e1a4f7836
Thumbelina <41448787+nsoeltan@users.noreply.github.com>
2019-03-15 20:27:57 -0400

    Deployment

--- server.js
+++ server.js
@@ -14,24 +14,24 @@ app.use(express.static("client/build"));
 app.use(routes);
 
 // Set up promises with mongoose
-mongoose.Promise = global.Promise;
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/releasenotes",
-  {
-    useMongoClient: true
-  }
-);
-
-
 // mongoose.Promise = global.Promise;
-
+// // Connect to the Mongo DB
 // mongoose.connect(
-//   process.env.MONGODB_URI || "",
+//   process.env.MONGODB_URI || "mongodb://localhost/releasenotes",
 //   {
 //     useMongoClient: true
 //   }
 // );
+
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://heroku_lk1n3656:a6b0c6q7o34a788e3b0eti3qva@ds155634.mlab.com:55634/heroku_lk1n3656",
+  {
+    useMongoClient: true
+  }
+);
 
 // Start the API server
 app.listen(PORT, function() {
